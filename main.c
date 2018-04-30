ede99fdec4ad24479511320b2cc6434d973c505d
Ian F <ianmichael.fennie@haw-hamburg.de>
2018-04-30 15:57:21 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdint.h>
+#include "tm4c1294ncpdt.h"
+
+void configSys(void);
+
+void dispayValue(void);
+
+int main(void) {
+
+    return 0;
+}
+
+void configSys(void)
+{
+
+}
+
+void dispayValue(void)
+{
+
+}
